abe600d9d11771617e4504439d9b23c78349634f
Marcelo Azevedo <marcelo@wpromote.com>
2014-05-28 09:13:17 -0700

    Define constants in db_default to connect to database

--- pdodb/db.php
+++ pdodb/db.php
@@ -1,10 +1,10 @@
 <?php
-
-define('MYDB_HOST','127.0.0.1');
-define('MYDB_NAME','wpromote');
-define('MYDB_USER','root');
-define('MYDB_PASS','esqueci22');
-define('DB_DEBUG',true);
+include_once('../db_default.php');
+//define('MYDB_HOST','127.0.0.1');
+//define('MYDB_NAME','wpromote');
+//define('MYDB_USER','root');
+//define('MYDB_PASS','esqueci22');
+//define('DB_DEBUG',true);
 
 
 

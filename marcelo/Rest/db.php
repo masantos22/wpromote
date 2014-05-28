287c8a678517ab86527b35f1e2377648570a8a50
masantos22 <masantos22@gmail.com>
2014-05-28 08:48:49 -0700

    Update db.php

--- marcelo/Rest/db.php
+++ marcelo/Rest/db.php
@@ -1,9 +1,9 @@
 <?php
 
-define('MYDB_HOST','127.0.0.1');
-define('MYDB_NAME','wpromote');
-define('MYDB_USER','root');
-define('MYDB_PASS','esqueci22');
+define('MYDB_HOST','');
+define('MYDB_NAME','');
+define('MYDB_USER','');
+define('MYDB_PASS','');
 define('DB_DEBUG',true);
 
 

5222c435a0a402cfbf3a1b84ce49d0fca96b3704
Marcelo Azevedo <marcelo@wpromote.com>
2014-05-29 08:37:53 -0700

    Remove .DS_Store

--- db_default.php
+++ db_default.php
@@ -1,7 +1,7 @@
 <?php
-define('MYDB_HOST','');
-define('MYDB_NAME','');
-define('MYDB_USER','');
-define('MYDB_PASS','');
+define('MYDB_HOST','127.0.0.1');
+define('MYDB_NAME','marcelo');
+define('MYDB_USER','root');
+define('MYDB_PASS','esqueci22');
 define('DB_DEBUG',true);
 ?>

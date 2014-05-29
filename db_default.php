1dc0b7b16d102d43900aefcf6bd98c05223e6222
masantos22 <masantos22@gmail.com>
2014-05-29 09:02:10 -0700

    Update db_default.php

--- db_default.php
+++ db_default.php
@@ -1,7 +1,7 @@
 <?php
-define('MYDB_HOST','127.0.0.1');
-define('MYDB_USER','root');
-define('MYDB_PASS','esqueci22');
-define('MYDB_NAME','marcelo');
+define('MYDB_HOST','');
+define('MYDB_USER','');
+define('MYDB_PASS','');
+define('MYDB_NAME','');
 define('DB_DEBUG',true);
 ?>
